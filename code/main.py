33e1d4fe03eaea0c9c9ba0dd612c4d236e8577bb
HunterZberg <68399121+HunterZberg@users.noreply.github.com>
2022-11-29 16:37:03 -0500

    created file structure

---
+++ code/main.py
@@ -0,0 +1,6 @@
+import numpy as np
+import tensorflow as tf
+from preprocessing import *
+
+if __name__ == "__main__":
+    data = preprocess()
---
+++ code/preprocessing.py
@@ -0,0 +1,16 @@
+import json
+import numpy as np
+import random
+
+def preprocess(filepath='data/', k=10000):
+    data = None
+    return data
+
+
+
+def randomly_sample(inputs, k):
+    return random.choices(inputs, k=k)
+
+
+if __name__ == "__main__":
+    preprocess()
