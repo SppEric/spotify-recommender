23452f24082b66a248fbd044f44ad2cc1b982e99
eric-j-han <ericjasonhan@gmail.com>
2022-11-29 17:53:08 -0500

    RNN and accuracy metric copied over

--- code/accuracy.py
+++ code/accuracy.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
 import numpy as np
+
+class SongAccuracy(tf.keras.losses.SparseCategoricalCrossentropy):  
+    def __init__(self, name='perplexity', from_logits=False, **kwargs):
+        super(SongAccuracy, self).__init__(name=name, from_logits=from_logits)
+    
+    def call(self, *args, **kwargs):
+        losses = super().call(*args, **kwargs)
+        return tf.math.exp(tf.math.reduce_mean(losses))
